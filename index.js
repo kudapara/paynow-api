8902d774db270f301740d972588050b02f0e6f8f
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-12-28 15:58:49 +0200

    Use the error handler module in the app.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(cors())
 const axios = require('./axios')
 const sha512 = require('./sha512')
 const uuidv4 = require('uuid/v4')
+const errorHandlers = require('./handlers');
 const transactions = require('./transactions')
 const paymentResponders = require('./payment-responders')
 
@@ -211,6 +212,21 @@ app.post('/pay/paynow', async (req, res) => {
   }
 })
 
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound);
 
-app.listen(3000, () => console.log('app running on 3000'))
+// One of our error handlers will see if these errors are just validation errors
+app.use(errorHandlers.flashValidationErrors);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors);
+
+const { PORT } = process.env
+app.listen(PORT, () => console.log('app running on ', PORT))
 
