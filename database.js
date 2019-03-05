949451e6f3aab600cce9acac36ec16d3f9bd677f
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2019-03-05 09:42:58 +0200

    USE THE CORRECT MONGO URI

--- database.js
+++ database.js
@@ -1,3 +1,3 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
-module.exports = mongoose
+const mongoose = require("mongoose");
+mongoose.connect(process.env.PAYNOW_MONGO_URI);
+module.exports = mongoose;
